d257eb9910df67239027429bc2e388cf6a616251
NaACE <36949980+NaACE@users.noreply.github.com>
2019-11-02 12:22:28 +0300

    new game

--- main.js
+++ main.js
@@ -3,40 +3,24 @@ array_filling();
 game.newLoop("game", function () {
   game.clear();
 
-  OOP.drawArr(wall);
+  OOP.drawArr(cobblestone);
   OOP.drawArr(field);
   OOP.drawArr(eats);
-  OOP.drawArr(cobblestone);
+  OOP.drawArr(wall);
 
-  pointJS.camera.setPositionC(player.getPosition());
+  setPositionC(player.getPosition());
   player.draw();
 
-  controlerPlayer();
-  playerPaintingBlocks();
-  playerTouchedFood();
+  controler_player();
+  check_what_player_stepped_on();
 
-  if (user_made_move == true) {
-    dropCobblestone();
-    dropFood();
-    dropBiasCobblestone();
-    dropBiasEats();
+  if(user_made_move == true) {
+    physica.fall_stones_and_food();
+    physica.movement_direction();
   }
-  if (userGoNextLvL() == true) {
-    nextLevel();
-    setUserGoNextLvL();
-    game.setLoop('game');
-  }
-  if(escape != null) {
-    escape.draw();
-    playerTouchedEscape();
-  }
-  if(antivirus.length >= 1) {
-    move_antivirus();
-    OOP.drawArr(antivirus);
-  }
+  
   brush.drawTextS({ text: 'count: ' + count, size: 32, color: '#FFFFFF', strokeWidth: 1, x: 10, y: 10, style: 'bold'});
 });
-console.log(antivirus[0].getPosition());
 
 game.setLoop("game");
 game.start();
